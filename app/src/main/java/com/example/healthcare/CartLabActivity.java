cfa3386a4df3efdb87c8534653c23a12dfafb1bc
Minn <nhatmin1230@gmail.com>
2024-01-26 23:08:14 +0700

    upload source code

--- app/src/main/java/com/example/healthcare/CartLabActivity.java
+++ app/src/main/java/com/example/healthcare/CartLabActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -23,9 +27,12 @@ public class CartLabActivity extends AppCompatActivity {
    ArrayList list;
    SimpleAdapter sa;
    TextView tvTotal;
+   ListView lst;
    private DatePickerDialog datePickerDialog;
    private TimePickerDialog timePickerDialog;
    private Button dateButton,timeButton,btnCheckout,btnBack;
+   private String[][] packages ={};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +43,46 @@ public class CartLabActivity extends AppCompatActivity {
         btnCheckout = findViewById(R.id.buttonCartCheckout);
         btnBack = findViewById(R.id.buttonCardBack);
         tvTotal = findViewById(R.id.textViewCartTotalCost);
+        lst = findViewById(R.id.listViewCart);
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username","").toString();
+
+        Database db = new Database(getApplicationContext(),"healthcare",null,1);
+        float totalAmuont = 0;
+        ArrayList dbData = db.getCartData(username,"lab");
+        //Toast.makeText(getApplicationContext(), ""+dbData, Toast.LENGTH_SHORT).show();
+
+        packages = new String[dbData.size()][];
+        for (int i = 0; i < packages.length ; i++) {
+            packages[i] = new String[5];
+        }
+
+        for (int i = 0; i < dbData.size() ; i++) {
+                String arrData = dbData.get(i).toString();
+                String[] strData = arrData.split(java.util.regex.Pattern.quote("$"));
+                packages[i][0] = strData[0];
+                packages[i][4] = "Cost: "+strData[1]+"/-";
+                totalAmuont = totalAmuont +Float.parseFloat(strData[1]);
+        }
+        tvTotal.setText("Total Cost: "+totalAmuont);
+
+        list = new ArrayList();
+        for (int i = 0;i<packages.length;i++){
+            item = new HashMap<String,String>();
+            item.put("line1", packages[i][0]);
+            item.put("line2", packages[i][1]);
+            item.put("line3", packages[i][2]);
+            item.put("line4", packages[i][3]);
+            item.put("line5", packages[i][4]);
+            list.add(item);
+        }
+        sa = new SimpleAdapter(this,list,
+                R.layout.multi_lines,
+                new String[] {"line1","line2","line3","line4","line5"},
+                new int[] {R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d, R.id.line_e});
+        lst.setAdapter(sa);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +90,18 @@ public class CartLabActivity extends AppCompatActivity {
                 startActivity(new Intent(CartLabActivity.this, LabTestActivity.class));
             }
         });
+
+        btnCheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it= new Intent(CartLabActivity.this,LabTestBookActivity.class);
+                it.putExtra("price",tvTotal.getText());
+                it.putExtra("date",dateButton.getText());
+                it.putExtra("time",timeButton.getText());
+                startActivity(it);
+            }
+        });
+
         initDatePicker();
         dateButton.setOnClickListener(new View.OnClickListener() {
             @Override
